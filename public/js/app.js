2b6b30fde29693483323fe5654855ca8613ed862
Affan Khan <coder.mak.29@gmail.com>
2024-03-18 06:54:40 +0530

    Set up app for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded.')
-
     const weatherForm = document.querySelector('form')
     const search = document.querySelector('input')
     const messageOne = document.querySelector('#message-1')
@@ -13,7 +11,7 @@ console.log('Client side javascript file is loaded.')
        const location = search.value
        messageOne.textContent="Loading..."
        messageTwo.textContent=''
-        fetch('http://localhost:3000/weather?address='+location).then((response) => {
+        fetch('/weather?address='+location).then((response) => {
             messageOne.textContent=''   
         response.json().then((data)=> {
                 if(data.error) {
